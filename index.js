e80a70400a52cda35d66c509e76dd81af43cde03
whb <whb@amazon.com>
2024-01-12 17:41:01 +0800

    init

--- index.js
+++ index.js
@@ -24,25 +24,40 @@ server.on("request", function (req, res) {
 //    });
 
     if(req.url === "/"){
-        fs.readFile("./index.html", "UTF-8", function(err, html){
+        fs.readFile("./test.html", "UTF-8", function(err, html){
             if (err) throw err;
             res.writeHead(200, {"Content-Type": "text/html"});
             res.end(html);
         });
     }else if(req.url.match("\.css$")){
-        var cssPath = path.join(__dirname, req.url);
+        var cssPath = path.join(__dirname, 'public', req.url);
         var fileStream = fs.createReadStream(cssPath, "UTF-8");
         res.writeHead(200, {"Content-Type": "text/css"});
         fileStream.pipe(res);
 
     }else if(req.url.match("\.png$")){
-        var imagePath = path.join(__dirname, req.url);
+        var imagePath = path.join(__dirname, 'public', req.url);
         var fileStream = fs.createReadStream(imagePath);
         res.writeHead(200, {"Content-Type": "image/png"});
         fileStream.pipe(res);
+    }else if(req.url.match("\.jpg$")){
+         var imagePath = path.join(__dirname, 'public', req.url);
+         var fileStream = fs.createReadStream(imagePath);
+         res.writeHead(200, {"Content-Type": "image/jpeg"});
+         fileStream.pipe(res);
+    }else if(req.url.match("\.gif$")){
+          var imagePath = path.join(__dirname, 'public', req.url);
+          var fileStream = fs.createReadStream(imagePath);
+          res.writeHead(200, {"Content-Type": "image/gif"});
+          fileStream.pipe(res);
+    }else if(req.url.match("\.js$")){
+           var imagePath = path.join(__dirname, 'public', req.url);
+           var fileStream = fs.createReadStream(imagePath);
+           res.writeHead(200, {"Content-Type": "text/javascript"});
+           fileStream.pipe(res);
     }else{
-        res.writeHead(404, {"Content-Type": "text/html"});
-        res.end("No Page Found");
+           res.writeHead(404, {"Content-Type": "text/html"});
+           res.end("No Page Found");
     }
 
 })
