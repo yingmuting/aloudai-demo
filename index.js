10c280378e41ab65c8eb8751b7160a59b0d0b3c1
whb <whb@amazon.com>
2024-01-12 10:30:01 +0800

    jpg

--- index.js
+++ index.js
@@ -29,14 +29,14 @@ server.on("request", function (req, res) {
             res.writeHead(200, {"Content-Type": "text/html"});
             res.end(html);
         });
-    }else if(req.url.match(".css$")){
-        var cssPath = path.join(__dirname, 'public', req.url);
+    }else if(req.url.match("\.css$")){
+        var cssPath = path.join(__dirname, req.url);
         var fileStream = fs.createReadStream(cssPath, "UTF-8");
         res.writeHead(200, {"Content-Type": "text/css"});
         fileStream.pipe(res);
 
-    }else if(req.url.match(".png$")){
-        var imagePath = path.join(__dirname, 'public', req.url);
+    }else if(req.url.match("\.png$")){
+        var imagePath = path.join(__dirname, req.url);
         var fileStream = fs.createReadStream(imagePath);
         res.writeHead(200, {"Content-Type": "image/png"});
         fileStream.pipe(res);
